98c97ece86f96aaa683cca61af3eaa578eeee919
Hexkritor <hexkritor@yandex.com>
2018-04-07 21:42:23 +0300

    Basic Version

---
+++ CalculationParser/CalculationParser/Calculations.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Xml.Schema;
+
+namespace CalculationParser
+{
+
+	public enum Operand {
+		NONE,
+		ADD,
+		SUBTRACT,
+		MULTIPLY,
+		DIVIDE
+	};
+
+
+	[XmlRoot("folder")]
+	public class Calculations {
+		[XmlElement ("folder")] public List<Calculation> calculations = new List<Calculation> ();
+	}
+
+	public class Calculation:IXmlSerializable {
+		public int[] hasElement { get; set; }
+		public string uid { get; set; }
+		public Operand operand { get; set; }
+		public int mod { get; set; }
+
+		public XmlSchema GetSchema () {return null;}
+		public void ReadXml (XmlReader reader) {
+			hasElement = new int[] {0 ,0, 0, 0};
+			reader.ReadStartElement ();
+			while (true) {
+				reader.MoveToContent ();
+				if (reader.NodeType != XmlNodeType.Element)
+					break;
+				switch (reader.GetAttribute ("name")) {
+				case "uid":
+					++hasElement [0];
+					uid = reader.GetAttribute ("value");
+					break;
+				case "operand":
+					++hasElement [1];
+					operand = string2operand(reader.GetAttribute ("value"));
+					break;
+				case "mod":
+					++hasElement [2];
+					mod = int.Parse(reader.GetAttribute ("value"));
+					break;
+				default:
+					++hasElement [3];
+					break;
+				}
+				reader.ReadElementContentAsString ();
+			}
+			reader.ReadEndElement ();
+		}
+		public void WriteXml(XmlWriter writer) {}
+		private Operand string2operand (string str) {
+			switch (str) {
+			case "add":
+				return Operand.ADD;
+			case "subtract":
+				return Operand.SUBTRACT;
+			case "multiply":
+				return Operand.MULTIPLY;
+			case "divide":
+				return Operand.DIVIDE;
+			default:
+				return Operand.NONE;
+			}
+		}
+	}
+
+}
+
---
+++ CalculationParser/CalculationParser/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace CalculationParser
+{
+
+	class MainClass
+	{
+		public static void Main (string[] args) {
+			//инициализация переменных для вывода файла с наименьшим количеством ошибок
+			string bestFile = "";
+			int bestFileErrors = int.MaxValue;
+			//проверка на количество аргументов
+			if (args.Length != 1) {
+				Console.WriteLine ("Для запуска программы укажите директорию, с которой хотите работать");
+				return;
+			}
+			//проверка директории указанной в аргументе программы
+			string path = args [0]; 
+			if (!Directory.Exists (path)) {
+				Console.WriteLine ("Указанной директории не существует");
+				return;
+			}
+			//работа с файлами в папке
+			string[] files = Directory.GetFiles (path);
+			//TODO добавить многопоточный парсинг
+			foreach (string file in files) {
+				//парсим только xml файлы
+				if (file.Substring (file.Length - 4).ToLower () == ".xml") {
+					double result = 0; //результат вычислений
+					int errors = 0; //счетчик ошибок
+					//десереализация xml
+					XmlSerializer xmlFile = new XmlSerializer (typeof(Calculations));
+					Calculations folder = new Calculations();
+					using (FileStream fs = new FileStream(file, FileMode.Open)) {
+						folder = (Calculations)xmlFile.Deserialize(fs);
+					}
+					//вычисления результата
+					foreach (Calculation calculation in folder.calculations) {
+						//проверка на валидность calculation 
+						if (!(calculation.hasElement [0] == 1 && calculation.hasElement [1] == 1 && calculation.hasElement [2] == 1 && calculation.hasElement [3] == 0)) {
+							errors++;
+							//TODO добавить вывод ошибок
+							continue;
+						}
+						//сами расчёты
+						switch (calculation.operand) {
+						case Operand.ADD:
+							result += calculation.mod;
+							break;
+						case Operand.SUBTRACT:
+							result -= calculation.mod;
+							break;
+						case Operand.MULTIPLY:
+							result *= calculation.mod;
+							break;
+						case Operand.DIVIDE:
+							//проверка делимости на нуль
+							if (calculation.mod == 0) {
+								++errors;
+								break;
+							}
+							result /= calculation.mod;
+							break;
+						}
+					}
+					//вывод результата
+
+					Console.WriteLine (file.Substring(file.LastIndexOf("\\")+1)+" Результат: "+result);
+					//проверка на минимум ошибок
+					if(bestFileErrors > errors) {
+						bestFile = file;
+						bestFileErrors = errors;
+					}
+				}
+			}
+			//вывод лучшего файла
+			Console.WriteLine("Файл с минимальным количеством ошибок: "+bestFile.Substring(bestFile.LastIndexOf("\\")+1)+" (Ошибок: "+bestFileErrors.ToString()+")");
+		}
+	}
+}
